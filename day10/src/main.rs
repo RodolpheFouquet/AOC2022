ed39edec349cf50d72cf502460d256d430a0e5c3
Rodolphe Fouquet <rodolphe@unicowd.com>
2023-09-13 01:45:38 +0100

    day 10 done

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 enum Command {
     Noop,
@@ -21,6 +23,40 @@ impl TryFrom<&str> for Command {
             )),
             _ => Err(()),
         }
+    }
+}
+
+struct Screen {
+    sprite: i32,
+    pixels: [[char; 40]; 6],
+}
+
+impl Screen {
+    fn new() -> Self {
+        Self {
+            sprite: 0,
+            pixels: [[' '; 40]; 6],
+        }
+    }
+
+    fn move_sprite(&mut self, pos: i32) {
+        self.sprite = pos;
+    }
+
+    fn visible(&self) -> (i32, i32, i32) {
+        (self.sprite - 1, self.sprite, self.sprite + 1)
+    }
+}
+
+impl Display for Screen {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.pixels.iter().for_each(|line| {
+            line.iter().for_each(|c| {
+                write!(f, "{}", c);
+            });
+            writeln!(f, "");
+        });
+        write!(f, "")
     }
 }
 
@@ -53,7 +89,24 @@ fn main() {
         .iter()
         .map(|&x| cycles[(x - 1) as usize] * x)
         .sum();
+
+    let mut screen = Screen::new();
     println!("{}", strength);
+    println!("{}", cycles.len());
+
+    for i in 0..6 {
+        for j in 0..40 {
+            screen.move_sprite(cycles[i * 40 + j]);
+            let (x1, x2, x3) = screen.visible();
+
+            if j as i32 == x1 || j as i32 == x2 || j as i32 == x3 {
+                screen.pixels[i][j] = '#';
+            } else {
+                screen.pixels[i][j] = '.';
+            }
+        }
+    }
+    println!("{}", screen);
 }
 
 #[cfg(test)]
